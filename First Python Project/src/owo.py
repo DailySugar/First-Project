8cff9c321e0d7b63ea6d71596e42b5dcc82ac69b
DailySugar <alexsunn30@gmail.com>
2021-09-23 10:58:14 -0400

    Update owo.py

--- First Python Project/src/owo.py
+++ First Python Project/src/owo.py
@@ -4,4 +4,18 @@ Created on Sep 18, 2021
 @author: alexs
 '''
 
-print("print"[:3]*3)
+import pydoc
+
+def test(listlists,target):
+    if type(listlists) is not list:
+        return None
+    else:
+        count=0
+        for l in listlists:
+            if type(l) is not list:
+                return None
+            else:
+                if target in l:
+                    count+=1
+        return count            
+print(test([[1,0],[2,3,4],[0,6,7,1,1,3],["w","n",1]],1))
