13a349d9ba25c39a33f1e48ac8cddbcefe8a4d2b
DailySugar <alexsunn30@gmail.com>
2021-09-29 14:11:53 -0400

    Update keys.py

--- First Python Project/src/keys.py
+++ First Python Project/src/keys.py
@@ -3,5 +3,21 @@ animals = {"Hudston":"dog","Mocha":"cat","Gus":"sloth"}
 print(animals)
 print(len(animals))
 
-for pet in animals:
+'''for pet in animals:
     print(pet,"is a", animals[pet])
+print(type(animals))'''
+
+str = {"abc":"awooo"}
+del str["abc"]
+#print(str)
+'''
+for k,v in iter(animals.items()):
+    print(v)
+    print(k)
+'''
+
+set = {1,2,3,4,5,6}
+set.add(5)
+set.update([7,8])
+
+print(set)
