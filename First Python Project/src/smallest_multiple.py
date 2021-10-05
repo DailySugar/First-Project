5fdeda6b719526a71aa920e2182cdbda90473301
DailySugar <alexsunn30@gmail.com>
2021-10-05 19:23:54 -0400

    Update smallest_multiple.py

--- First Python Project/src/smallest_multiple.py
+++ First Python Project/src/smallest_multiple.py
@@ -1,10 +1,12 @@
-num=20
-output=num
-increment=num
+def smallestmultiple(num):
+    "Finds smallest number that can be divided by all the numbers 'num' and below"
+    output=num
+    increment=num
+    for x in range(1,num):
+        while output % (num - x) > 0:
+            output+=increment
+        increment=output
+    return output
 
-for x in range(1,num):
-    while (output)%(num-x)!=0 or output//(num-x)%2!=0:
-        output+=increment
-    increment=output
-    print(num-x,output)
-print(output)
+
+print(smallestmultiple(20))
