aa371bd606b6007cbe3b002df4cd32d2262818fc
DailySugar <alexsunn30@gmail.com>
2021-10-05 19:23:49 -0400

    Delete optimized_assignment.py

--- First Python Project/src/optimized_assignment.py
+++
@@ -1,5 +0,0 @@
-text,key,output="abc","zyx",""
-table="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
-for x in range(len(text)):
-    output+=table[table.index(text[x])+table.index(key[x])]
-print("\nYour encrypted text is:",output)
