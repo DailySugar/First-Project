27eef492db505c8c268b6666c6055f1b6f937dd4
DailySugar <alexsunn30@gmail.com>
2021-10-10 15:10:49 -0400

    uwu

--- First Python Project/src/largest_prime_factor.py
+++ First Python Project/src/largest_prime_factor.py
@@ -1,20 +1,40 @@
-'''
+"""
 Created on Oct 2, 2021
 
 @author: alexs
-'''
+"""
+import math
 
-def check(num,lis):
+
+# The prime #s database starts empty. When the starting number, 2,
+# is tested, nothing in the database can divide 2, so 2 gets added
+# to the prime numbers list. The main idea here is that all divisible
+# numbers out there are ultimately divisible by prime numbers.
+nums = [2]
+
+
+def check(num, lis = nums) -> bool:
     # Check if the # can be divided by something from the prime numbers database
     for y in lis:
-        if num%y==0:
+        if num % y == 0:
+            return False
+        if y > math.sqrt(num):
             return True
+    return True
 
-def largest_prime_factor(num,test):
-    nums=[]
-    for x in range(2,num+1):
-        if check(x,nums):
-            # Not prime, move onto next number
-            continue
-        # Add to the prime numbers database
-        nums.append(x)
+
+num = 600851475143
+# Without num+1, it'll only go up to 999 instead of 1000
+for x in range(3, round(math.sqrt(num)), 2):
+    if check(x) == False:
+        # Not prime, move onto next number
+        continue
+    # Add to the prime numbers database
+    nums.append(x)
+
+for x in range(1, num // 2, 2):
+    if num % x == 0:
+        print(num // x, check(num // x))
+    if num % x == 0 and check(num // x):
+        print(num // x)
+        break
--- First Python Project/src/prime_numbers.py
+++ First Python Project/src/prime_numbers.py
@@ -3,21 +3,26 @@ Created on Sep 17, 2021
 
 @author: alexs
 '''
+import math
+
 
 def check(num,lis):
     # Check if the # can be divided by something from the prime numbers database
     for y in lis:
         if num%y==0:
             return True
+        if y > math.sqrt(num):
+            return False
+    return False
         
-num=1000
+num=600851475143
 # The prime #s database starts empty. When the starting number, 2,
 # is tested, nothing in the database can divide 2, so 2 gets added
 # to the prime numbers list. The main idea here is that all divisible
 # numbers out there are ultimately divisible by prime numbers.
-nums=[]
+nums=[2]
 # Without num+1, it'll only go up to 999 instead of 1000
-for x in range(2,num+1):
+for x in range(3,num+1,2):
     if check(x,nums):
         # Not prime, move onto next number
         continue
