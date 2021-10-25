9a5a5bd9052a606aa57584d991ad092517539cd2
DailySugar <alexsunn30@gmail.com>
2021-10-25 12:30:44 -0400

    uwu

--- First Python Project/Notes/algorithm.py
+++ First Python Project/Notes/algorithm.py
@@ -2,28 +2,55 @@
 # First phd thesis on modern computers. Founded school of computing in Queens
 
 # U.K.L. - "We read to find out who we are."
+# Everleest Gelwath
+# "I'm gonna die in a duel"
+# Wrote great math letter before dying in duel
+# The best science books are the ones which the authors make clear how much they don't know.
 
 import random
 
+# October 25, 2021
+# Sorting functions
+# Merge Sort: split into two equal sides and sort both sides, then merge both sides
+def m_s(list, start, end):
+    if start > end:
+        return []
+    if start == end:
+        return [list(start)]
+    elif start - end == 1:
+        return [list(start), list(end)] if list(start) < list(end) else [list(end), list(start)]
+    else:
+        left = m_s(list, start, (start + end) // 2)
+        right = m_s(list, (start, end) // 2, end)
+        new_list = []
+        for x in range(len(left)):
+            for y in right[x:]:
+                if y <
+
+
+def merge_sort(list):
+    return m_s(list, 0, len(list))
+
+
 # October 21, 2021
 # Binary search
-def b_s_r(list, target, first, last):
-    if first <= last:
-        mid = (first + last) // 2
-        if list[mid] == target:
-            return mid
-        elif list[mid] > target:
-            return b_s_r(list, target, first, mid - 1)
-        else:
-            return b_s_r(list, target, mid + 1, last)
-    return - 1
-
-
-def binary_search(list, target):
-    return b_s_r(list, target, 0, len(list) - 1)
-
-
-print(binary_search([1,2,3,4,5,6,7,8,9,10], 9))
+# def b_s_r(list, target, first, last):
+#     if first <= last:
+#         mid = (first + last) // 2
+#         if list[mid] == target:
+#             return mid
+#         elif list[mid] > target:
+#             return b_s_r(list, target, first, mid - 1)
+#         else:
+#             return b_s_r(list, target, mid + 1, last)
+#     return - 1
+#
+#
+# def binary_search(list, target):
+#     return b_s_r(list, target, 0, len(list) - 1)
+#
+#
+# print(binary_search([1,2,3,4,5,6,7,8,9,10], 9))
 
 #October 18, 2021
 
