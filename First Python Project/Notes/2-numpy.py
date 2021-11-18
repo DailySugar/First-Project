3543715cafbe88004a6bf47e6706178e4a440b05
DailySugar <alexsunn30@gmail.com>
2021-11-18 13:20:44 -0500

    uwu

---
+++ First Python Project/Notes/2-numpy.py
@@ -0,0 +1,24 @@
+# M.A.
+# "All stories are about wolves."
+
+# Nov 18 - numpy
+# table1 = np.loadtxt("test.txt", dtype = int)
+# print(table1)
+
+# table2 = np.loadtxt("test.txt", dtype = int, skiprows=1, usecols=(0,2,4,5)
+# print(table2)
+
+# table3 = np.loadtxt("test.txt", dtype = int, skiprows=1, usecols=range(3,6)
+# print(table3)
+
+# table4 = np.array([1,2,3,4])
+# print(table4)
+
+# table5 = np.zeros((5,4))
+# table6 = np.ones((3,4), dtype = int)
+# table7 = np.empty((2,5))
+
+# print(table6.shape)
+# print(np.sum(table6))
+# print(np.sum(table6[1]))
+# table5[1,2] = 99
