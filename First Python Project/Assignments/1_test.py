8a1f6eb1d99d3e953b86c2cf8f392b9aaf9ddebd
DailySugar <alexsunn30@gmail.com>
2021-11-24 12:44:31 -0500

    uwu

--- First Python Project/Assignments/1_test.py
+++ First Python Project/Assignments/1_test.py
@@ -1,7 +1,4 @@
 dict1 = {1:[[1,2,3],{1,2,3}],2:2,3:3}
-dict2 = dict1.copy()
-dict2[1][0] = dict1[1][0].copy()
 
-dict1[1][0].append("ayo")
-
-print(dict2,dict1)
+for x in dict1.keys():
+    print(dict1[x])
--- First Python Project/Assignments/Assignment_7.py
+++ First Python Project/Assignments/Assignment_7.py
@@ -1,13 +1,19 @@
 """
+Created November 21, 2021
+Alex Sun
+20289530
+CISC-121
 
-
+Using the k-means clustering algorithm with the Manhattan or
+Euclidean distance metric to group similar animals together.
 """
 
 from pathlib import Path
 import numpy as np
 from random import randint
+# Thanks to Q#5105517 on stack overflow for mentioning deepcopy
 from copy import deepcopy
-# Thanks to Q#5105517 on stack overflow for mentioning this
+from math import sqrt
 
 properties = 15
 animals_dict = {}
@@ -21,11 +27,9 @@ for line in (open(animals_file)).readlines()[1:]:
 
 animals_table = np.loadtxt(animals_file, dtype = int, skiprows=1, usecols=range(1,properties + 1))
 
-# print(animals_dict)
-
 
 def smallest(differences):
-    """Returns the index of the smallest difference."""
+    """Returns the index of the smallest number in the list."""
     smallest = differences[0]
     for x in differences[1:]:
         if x < smallest:
@@ -35,20 +39,39 @@ def smallest(differences):
 
 
 def manhattan(list1, list2):
+    """Computes and returns the Manhattan Metric distance from
+    two equal length lists."""
     total_difference = 0
     for x in range(len(list1)):
         total_difference += abs(list1[x] - list2[x])
     return total_difference
 
 
-def cluster_stable(old, new):
-    for x in range(len(old)):
-        if abs(old[x] - new[x]) > 0.1:
+def euclidean(list1, list2):
+    """Computes and returns the Euclidean distance from
+    two equal length lists."""
+    total_difference = 0
+    for x in range(len(list1)):
+        total_difference += (list1[x] - list2[x]) ** 2
+    return sqrt(total_difference)
+
+
+def cluster_stable(old_groups, new_groups):
+    """Checks and returns true if the two groups have the same animals, and
+    all attributes have a difference less than 0.1 between the two groups."""
+    attributes = list(range(len(old_groups[0][1])))
+    for group in old_groups.keys():
+        if old_groups[group][0] != new_groups[group][0]:
             return False
+        for attribute in attributes:
+            if abs(old_groups[group][1][attribute] - new_groups[group][1][attribute]) > 0.1:
+                return False
     return True
 
 
-def k_means(animals_table, group_count = 7, loops = 7, distance_algorithm = manhattan):
+def k_means(animals_table, group_count = 7, distance_algorithm = manhattan):
+    """Groups the animals in animals_table into different groups
+    using the distance algorithm chosen (manhattan, or euclidean)"""
     max_iterations = 100
     animals_index_count = len(animals_table) - 1
     groups = {}
@@ -56,6 +79,7 @@ def k_means(animals_table, group_count = 7, loops = 7, distance_algorithm = manh
     animals = set(range(0, animals_index_count + 1))
     attributes_count = len(animals_table[0])
 
+    # Build initial centres with their associated animals
     group_index = 0
     while len(initial_animals) < group_count:
         temp_animal = randint(0, animals_index_count)
@@ -66,66 +90,68 @@ def k_means(animals_table, group_count = 7, loops = 7, distance_algorithm = manh
             group_index += 1
         else:
             continue
-    # print("Initial Groups:", initial_animals)
+    # print("Initial Groups:", groups)
 
+    # Exclude initial centre animals from first iteration, as they are already sorted
     unsorted = animals.difference(initial_animals)
     for x in range(max_iterations):
         # print("Loop:", x + 1)
         for animal in unsorted:
             differences = []
+            # Determining the animal's group using the distance algorithm chosen
             for compare in range(group_count):
                 differences.append(distance_algorithm(animals_table[animal], groups[compare][1]))
+            #     print("Group:   ", compare)
+            #     print("Animal:  ", animals_table[animal], animal, animals_dict[animal])
+            #     print("Compare: ", groups[compare][1])
+            #     print("Distance:", distance_algorithm(animals_table[animal], groups[compare][1]))
+            # print("Chosen Group:", smallest(differences))
             groups[smallest(differences)][0].append(animal)
+        # Updating centre values for each group
         for group_index in range(group_count):
+            # print("Group:     ", group_index)
             group = groups[group_index][0]
             group_length = len(group)
+            # Try and except in case group is empty
             try:
                 updated_centre = animals_table[group[0]]
             except:
                 continue
-            # if group_index == 0:
-            #     # print("Animals in group", group_index, ":", group_length)
-            #     print(groups[0])
             for animal_index in range(1, group_length):
                 # Credits to Q#14050824 on stack overflow for zip() to sum the contents of two lists
                 updated_centre = [sum(attribute) for attribute in zip(*[updated_centre, animals_table[group[animal_index]]])]
-            # if group_index == 0: print(updated_centre)
+            # print("Center Sum: ", updated_centre)
+            # print("# Animals:  ", group_length, len(groups[group_index][0]))
             updated_centre = [attribute / group_length for attribute in updated_centre]
-            # if group_index == 0: print(updated_centre)
+            # print("Updated:    ", updated_centre)
             groups[group_index][1] = updated_centre
+        # print("Group After:", groups)
 
         # After first iteration, include the initial animals
         if x == 0:
             unsorted = animals
         else:
-            loop_again = 0
-            for group in range(group_count):
-                if old_groups[group] != groups[group]:
-                    loop_again = 1
-                    break
-                if not cluster_stable(old_groups[group][1], groups[group][1]):
-                    loop_again = 1
-                    break
-            if loop_again == 0:
+            # Start checking for stability after first iteration
+            # Stop looping if clustering algorithm has reached stability
+            if cluster_stable(old_groups, groups):
                 print("Stop looping at", x + 1)
                 # print(groups)
                 # print(old_groups)
                 return groups
         old_groups = deepcopy(groups)
-        if x < max_iterations - 1:
-            for group in range(group_count):
-                groups[group][0] = []
-    return groups
+        for group in range(group_count):
+            groups[group][0] = []
+    return old_groups
+
 
 def main():
+    # groups = k_means(animals_table, distance_algorithm = euclidean)
     groups = k_means(animals_table, distance_algorithm = manhattan)
     for group in range(len(groups)):
         print("Group", group + 1, ":", end = " ")
         for animal in groups[group][0]:
             print(animals_dict[animal], end = " ")
         print()
-    # print(animals_table[10])
-    # print(animals_table[49])
 
 
 main()
